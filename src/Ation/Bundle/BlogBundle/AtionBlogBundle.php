abc08717c272a931988a17fcec45c2a9d9d563c9
Michael Antoine <mantoine@linagora.com>
2014-02-25 16:52:24 +0100

    Create first blog bundle for site + add bootsrap framework. No task.

---
+++ src/Ation/Bundle/BlogBundle/AtionBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ation\Bundle\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AtionBlogBundle extends Bundle
+{
+}
---
+++ src/Ation/Bundle/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ation\Bundle\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('AtionBlogBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
